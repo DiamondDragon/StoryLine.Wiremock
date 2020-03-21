4b426af20eb1a0a3f2239d11a4aff5645ad00675
Diamond-Dragon <diamond_dragon@tut.by>
2020-03-21 10:37:26 +0300

    Retries added to library

--- StoryLine.Wiremock/Expectations/HttpRequestMockExpectation.cs
+++ StoryLine.Wiremock/Expectations/HttpRequestMockExpectation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using StoryLine.Contracts;
 using StoryLine.Exceptions;
 using StoryLine.Wiremock.Builders;
@@ -8,19 +9,43 @@ namespace StoryLine.Wiremock.Expectations
     public class HttpRequestMockExpectation : IExpectation
     {
         private readonly IApiStubState _state;
+        private readonly int _retryCount;
+        private readonly TimeSpan _retryInterval;
 
-        public HttpRequestMockExpectation(IApiStubState state)
+        public HttpRequestMockExpectation(
+            ApiStubState state, 
+            int retryCount, 
+            TimeSpan retryInterval)
         {
+            if (retryCount <= 0) throw new ArgumentOutOfRangeException(nameof(retryCount));
+
             _state = state ?? throw new ArgumentNullException(nameof(state));
+            _retryCount = retryCount;
+            _retryInterval = retryInterval;
         }
 
         public void Validate(IActor actor)
         {
-            
-            var count = Config.Client.Count(_state.RequestState);
+            var count = WaitForMatchingRequestCount();
 
-            if (!_state.RequestCount.Evaluate(count))
+            if (!count)
                 throw new ExpectationException($"Expected Api to be called '{_state.RequestCount.Description}' but was called '{count}'");
+        }
+
+        private bool WaitForMatchingRequestCount()
+        {
+            for (var i = -1; i < _retryCount; i++)
+            {
+                var count = Config.Client.Count(_state.RequestState);
+
+                var hasMatchingRequestCount = _state.RequestCount.Evaluate(count);
+                if (hasMatchingRequestCount)
+                    return true;
+
+                Thread.Sleep(_retryInterval);
+            }
+
+            return false;
         }
     }
 }
